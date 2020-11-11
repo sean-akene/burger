c6c28d5a028f5cdd319b133d6048f7f225e399ad
Sean Delaney <seandarren96@gmail.com>
2020-11-10 22:00:14 -0500

    Mysql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("megabacon");
+INSERT INTO burgers (burger_name, devoured) VALUES ("beefanator");
+INSERT INTO burgers (burger_name, devoured) VALUES ("kentucky classic");
+INSERT INTO burgers (burger_name, devoured) VALUES ("all-da-smoke");
