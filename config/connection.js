8fdb6f632e03c3f9813b117f9564f76adbc05539
Sean Delaney <seandarren96@gmail.com>
2020-11-14 14:22:25 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
-  conncection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
